645f197b7d1aa272f8ba84aa4d0e5c6798307397
Alfredo Barragan <64043214+PastaChicken@users.noreply.github.com>
2025-10-03 02:33:58 +0000

    Submission for S01 by Alfredo

--- src/main.ts
+++ src/main.ts
@@ -23,4 +23,5 @@ button.addEventListener("click", () => {
   // This looks like to a good place to add some logic!
   console.log("I have these thingies:", button, counterElement, counter);
   counter++;
+  counterElement.innerText = counter.toString();
 });
